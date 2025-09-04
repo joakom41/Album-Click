0b3b758c075748dd8af97aa0f865fed49a1f29b3
Joaquin Sadoc <joakom41@gmail.com>
2025-09-03 22:50:36 -0400

    ...

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from core import views 
-
-urlpatterns = [
-    path('',views.index,name="index"),
-    path('album/',views.album,name="album"),
-    path('admin/', admin.site.urls),
-]
--- miweb/urls.py
+++ miweb/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from core import views 
 
 urlpatterns = [
+    path('',views.index,name="index"),
+    path('album/',views.album,name="album"),
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
 ]
-
-# Servir archivos estáticos durante el desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
